9d61ecbc61ee1e0517f618cd36158e0c0ec77475
Sushant Jadhav <sushant.jadhav@sequel.com>
2021-04-06 09:54:53 +0530

    Randomize CSS classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
-        <Header />
-        <MarketingApp />
-      </BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <BrowserRouter>
+          <Header />
+          <MarketingApp />
+        </BrowserRouter>
+      </StylesProvider>
     </div>
   );
 };
